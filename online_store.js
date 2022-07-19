6300691f0efa0f08df45edf93d5aad25fe6ca27f
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-07-19 20:25:43 +0400

    feat: initialize class Good and its methods

---
+++ online_store.js
@@ -0,0 +1,13 @@
+class Good {
+    constructor(id, name, description, sizes, price, available) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.sizes = sizes;
+        this.price = price;
+        this.available = available;
+    }
+    setAvailable(status) {
+        this.available = status;
+    }
+}
